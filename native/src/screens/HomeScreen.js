61803c2b65ca98eefdc763e9817df79da5fae221
Joshua Jones <49686077+JoshuaJoness@users.noreply.github.com>
2020-04-25 10:54:09 -0400

    Now retrieiving posts via useEffect hook

--- native/src/screens/HomeScreen.js
+++ native/src/screens/HomeScreen.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text, StyleSheet, FlatList } from 'react-native'
+import axios from 'axios'
 
 const HomeScreen = () => {
+  useEffect(() => {
+    axios.get('http://192.168.2.241:4000/posts').then(res => {
+      console.log(res.data);
+    }).catch(err => {
+        console.log(err);
+      })
+   }, [])
+
   return(
     <View>
       <Text>Home Screen</Text>
