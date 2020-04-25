b3816c306201467472d507c6cdb05ebd786c16f0
Joshua Jones <49686077+JoshuaJoness@users.noreply.github.com>
2020-04-25 11:16:14 -0400

    Now displaying a list of posts
    -used FlatList
    -showing title, summary, author, publish date
    -used regex to summarize paragraph

--- native/src/screens/HomeScreen.js
+++ native/src/screens/HomeScreen.js
@@ -1,11 +1,17 @@
-import React, { useEffect } from 'react'
-import { View, Text, StyleSheet, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
 import axios from 'axios'
 
+const regex = /.*?(\.)(?=\s[A-Z])/;
+
 const HomeScreen = () => {
+  const [posts, setPosts] = useState([])
+
+
   useEffect(() => {
     axios.get('http://192.168.2.241:4000/posts').then(res => {
       console.log(res.data);
+      setPosts(res.data)
     }).catch(err => {
         console.log(err);
       })
@@ -14,10 +20,31 @@ const HomeScreen = () => {
   return(
     <View>
       <Text>Home Screen</Text>
+      <FlatList
+        data={posts}
+        keyExtractor={(post) => post.title}
+        renderItem={({item}) => {
+          return (
+            <TouchableOpacity style={styles.post}>
+              <View>
+                <Text>{item.title}</Text>
+                <Text>{regex.exec(item.body)}</Text>
+                <Text>{item.author.name}</Text>
+                <Text>{item.publishedAt}</Text>
+              </View>
+            </TouchableOpacity>
+          )
+         }}
+       />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  post: {
+    borderWidth: 2,
+    borderColor: 'black'
+}
+})
 
 export default HomeScreen
