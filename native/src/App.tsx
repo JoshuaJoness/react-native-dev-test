6abfab90789838196a6ea5a3a576f2d350fb5652
Joshua Jones <49686077+JoshuaJoness@users.noreply.github.com>
2020-04-25 10:11:53 -0400

    Created HomeScreen, added additional dependencies

--- native/src/App.tsx
+++ native/src/App.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import HomeScreen from './screens/HomeScreen.js'
 
-export function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
   },
-});
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      title: 'Blogs'
+    }
+  }
+)
+
+export const App = createAppContainer(navigator)
